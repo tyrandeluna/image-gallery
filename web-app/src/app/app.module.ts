f9901c27c3ee5c526c1330b6d792479414cf10fe
Maria Beatriz de Almeida Batista <mb.almeidadv@hotmail.com>
2021-05-04 21:23:12 -0300

    created shared module

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -1,24 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ManterGaleriaComponent } from './galeria/manter-galeria/manter-galeria.component';
 import { HomeCarouselComponent } from './home/home-carousel/home-carousel.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ManterGaleriaComponent,
     HomeCarouselComponent,
-    HeaderComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
